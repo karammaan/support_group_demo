4cd11b652010ad50c90e4d93c2bdb3508bd035b3
karam <karammann98@gmail.com>
2017-03-27 20:43:28 +0530

    profile layout added

--- app/src/main/java/com/example/karam/demo1/home_layout.java
+++ app/src/main/java/com/example/karam/demo1/home_layout.java
@@ -1,7 +1,10 @@
 package com.example.karam.demo1;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class home_layout extends AppCompatActivity {
 
@@ -9,5 +12,27 @@ public class home_layout extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.home_layout);
+        Button login= (Button) findViewById(R.id.profile_layout);
+
+        final View.OnClickListener onbtn_click= new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(v.getId() == R.id.profile_layout)
+                {
+
+                    Intent i = new Intent(home_layout.this , profile_edit_layout.class);
+
+
+                    startActivity(i);
+
+
+                }
+
+
+            }
+        };
+
+        login.setOnClickListener(onbtn_click);
     }
 }
+
